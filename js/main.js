085c401b1dd1907e2ec3b36729c1f07e9e8a6a2c
cvdgp <57316995+cvdgp@users.noreply.github.com>
2022-02-19 00:41:29 +0100

    modifica script

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-/*1. confirmamos si el resto de dividir el número entre 3 y 5 es 0*/
+/*  1. Confirmamos si el resto de dividir el número entre 3 y 5 es 0. 
+    2. Si el resto es igual a 0  almacenamos el número.
+    3. Si el resto es igual a 0 lo sumamos.
+    4. Imprimimos el sumatorio en un div.
+    5. Imprimimos los múltiplos de 5 y 3 en divs que se posicionarán uno al lado del otro.
+*/
 
  let resultado = 0;
  let num =[];
