7781d78275f756ed294f38b309af608678a81f66
cvdgp <57316995+cvdgp@users.noreply.github.com>
2022-02-19 00:21:48 +0100

    En este punto cargan correctamente los globoa y el sumatorio de globos

---
+++ js/constantes.js
@@ -0,0 +1,5 @@
+export let resultado = 0;
+export let num =[];
+export let textoIntroducido = "";
+export let cargadorTxt     =   document.getElementById("txt");
+export let resultadoSuma   =   document.getElementById("resultado");
--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
 /*1. confirmamos si el resto de dividir el número entre 3 y 5 es 0*/
 
-let num =[];
-let resultado = 0, textoIntroducido = "";
-let cargadorTxt     =   document.getElementById("txt");
-let resultadoSuma   =   document.getElementById("resultado");
+ let resultado = 0;
+ let num =[];
+ let textoIntroducido = "";
+ let cargadorTxt     =   document.getElementById("txt");
+ let resultadoSuma   =   document.getElementById("resultado");
 
 
 for(let i = 1 ; i < 1000; i++ ){
     if(i%3 == 0) num.push(i);
     if(i%5 == 0) num.push(i);
 }
-console.log(num);
-
-
-
 num.forEach((element)=>{
     resultado += element;
     textoIntroducido += "<div class=´cargador´>" + element + "</div>";
 
 }); 
 
-resultadoSuma.innerHTML = resultado;
+resultadoSuma.innerHTML = "La suma de todos los números es:  "  +  resultado;
 cargadorTxt.innerHTML =  textoIntroducido;
